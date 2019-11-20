540d5acec699226a9db5ec14c878dd8c54e80652
fogdingding <fogdingding@gmail.com>
2019-11-21 00:12:10 +0800

    for issue_#1

--- gaipy/__init__.py
+++ gaipy/__init__.py
@@ -1,1 +1,12 @@
 from .gaisdb import *
+
+__all__ = [
+        'Create',
+        'Show',
+        'Drop',
+        'Insert',
+        'Update',
+        'Select',
+        'Search',
+        'Del',
+    ]
